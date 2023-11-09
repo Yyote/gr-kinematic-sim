e58f97918abf59b50475d62522e1d54292f1f9c1
Andrey <yyootttaa@gmail.com>
2023-11-09 22:50:57 +0300

    Lidar functions working. Refactoring into class required

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-from custom_utils.sensors import draw_lidar_around_point, check_collisions_between_tilemap_and_lines
+from custom_utils.sensors import get_lidar_lines_around_point, check_collisions_between_tilemap_and_lines
 from custom_utils.physics import check_collisions_between_tilemap_and_spritelist, check_collisions_in_spritelist
 from custom_utils.object_tools import Sprite, TiledMap, PhysicalObject
 from custom_utils.gametools import handle_key_events, handle_offset_change, handle_keypresses_through_force, draw_every_sprite_in_list, scroll_screen_with_mouse
@@ -54,12 +54,13 @@ while running:
     
     handle_key_events()
     
-    lines = draw_lidar_around_point(screen, sprite1.rect.x + sprite1.rect.width / 2, sprite1.rect.y + sprite1.rect.height / 2, 30, 100)
-    collisions = check_collisions_between_tilemap_and_lines(gmap, lines)
+    lines = get_lidar_lines_around_point(screen, sprite1.rect.x + sprite1.rect.width / 2, sprite1.rect.y + sprite1.rect.height / 2, 40, 200, sprite1._current_rotation, offset_x=global_offset_x, offset_y=global_offset_y, draw_lines=True)
+    collisions = check_collisions_between_tilemap_and_lines(screen, gmap, lines)
     
     for i in range(len(collisions)):
         if collisions[i] is not None:
-            pg.draw.circle(screen, (255, 0, 0), collisions[i], 3, 3)
+            p = (collisions[i][0] + global_offset_x, collisions[i][1] + global_offset_y)
+            pg.draw.circle(screen, (255, 0, 0), p, 3, 3)
     
     global_offset_x, global_offset_y = handle_offset_change(global_offset_x, global_offset_y)
     global_offset_x, global_offset_y = scroll_screen_with_mouse(width, height, global_offset_x, global_offset_y)
